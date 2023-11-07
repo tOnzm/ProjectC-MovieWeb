83f93163255c9c0d34ca82e83b7dd8c9c99a94e0
tOnzm <tonselaman@gmail.com>
2023-11-07 21:59:17 +0700

    Update 21:59 071166

--- components/slideBox/recommentCard.vue
+++ components/slideBox/recommentCard.vue
@@ -3,7 +3,7 @@
       <v-sheet class="mx-auto" max-width="100%" style="background: transparent">
         <div class="slide-group-title">
           <h1>แนะนำ</h1>
-          <viewmoreBtn />
+          <ViewMoreBtn/>
         </div>
         <v-slide-group class="pa-4 slide-group-thumnail" show-arrows>
           <v-slide-group-item v-for="item in movie" :key="item.thumbnail">
@@ -17,10 +17,8 @@
   </template>
   
   <script>
-import viewmoreBtn from '../Buttons/viewmoreBtn.vue';  
   export default {
     name: "RecommentMovie",
-    components: { viewmoreBtn},
   
     data: () => ({
       movie: [
